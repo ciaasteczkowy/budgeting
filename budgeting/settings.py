23af2f8982cc352837444fcc17790c6157090912
ciaasteczkowy@gmail.com <ciaasteczkowy@gmail.com>
2016-08-31 17:51:38 +0200

    django!

--- budgeting/settings.py
+++ budgeting/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
